0578f3d6191c1d7a3b8ae271899b099c4ce0d65c
dev-ptera <dev.ptera@gmail.com>
2021-11-23 17:00:02 -0500

    Bring it on 2

--- server/src/config/banano/app.config.ts
+++ server/src/config/banano/app.config.ts
@@ -21,7 +21,8 @@ export const MANUAL_PEER_MONITOR_URLS = BAN_NODE_MONITORS;
 export const LEDGER_LOCATION = '/representatives/batman/BananoData/data.ldb';
 export const HOST_NODE_NAME = 'batman';
 export const BACKUP_NODES = [
-    'http://159.69.198.59:7070'
+    'http://159.69.198.59:7070',
+    'https://api-beta.banano.cc/'
     //   'https://banano-api.mynano.ninja/rpc',
     //   'https://api-beta.banano.cc/'
 ];
