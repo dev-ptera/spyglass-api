c2625993795338dd76d052dcd54ab2315403a323
dev-ptera <dev.ptera@gmail.com>
2021-11-28 14:14:00 -0500

    Add new dev funds

--- server/src/config/banano/app.config.ts
+++ server/src/config/banano/app.config.ts
@@ -1,4 +1,3 @@
-import { NanoClient } from '@dev-ptera/nano-node-rpc';
 import { BAN_KNOWN_VANITIES } from './known-vanities';
 import { BAN_KNOWN_ACCOUNTS } from './known-accounts';
 import { BAN_NODE_MONITORS } from './node-monitors';
@@ -11,12 +10,6 @@ export const URL_WHITE_LIST = [
     'https://www.spyglass-api.web.app',
 ];
 
-export const NANO_CLIENT = new NanoClient({
-    url: process.env.RPC_URL,
-    requestHeaders: {
-        Authorization: process.env.RPC_AUTH || '',
-    },
-});
 export const MANUAL_PEER_MONITOR_URLS = BAN_NODE_MONITORS;
 export const LEDGER_LOCATION = '/representatives/batman/BananoData/data.ldb';
 export const HOST_NODE_NAME = 'batman';
@@ -28,8 +21,18 @@ export const BURN_ADDRESSES = [
     'ban_1ban116su1fur16uo1cano16su1fur16161616161616161616166a1sf7xw',
 ];
 export const DEVELOPER_FUNDS = [
+    'ban_1grapemjtr5n684bu1x38th57x3te8qt6xpsyusjyi4s3u1zdoh8s1czfjz4',
+    'ban_1app1es6zce5943ydasp5r5ma77cdcqt6be8qz7f88woayuqstzrjjmob4eb',
+    'ban_3gojim9wh3t9w5aa8nhjbmkue8mes9frmrhy6wpqb34ikajw8h39hnbbap31',
+    'ban_1acaih1rhhczkfayd3iadpjroyfbbrzm1jrkx77qfep7fnuzky7mmwzx4544',
+    'ban_1ycheefobbddbde1p7874ky4ifiwebfuabkyqptunwwk84z3rgzktbqeo9fk',
     'ban_3fundbxxzrzfy3k9jbnnq8d44uhu5sug9rkh135bzqncyy9dw91dcrjg67wf',
     'ban_1fundm3d7zritekc8bdt4oto5ut8begz6jnnt7n3tdxzjq3t46aiuse1h7gj',
+    'ban_3kiwizqifxokn47pp6fh5jmytoaiaeynjhx4u5r6ug13apx3345enf4mr1ep',
+    'ban_3carobzdy3ah8pq1xzn38jkc46ozuu8qfx7eqzr8nyiy5yefwbaua6rkh3of',
+    'ban_1pearw95xajkzq1nmo6cixo1ugijk6gpm1ifwyegz6un4mt71qb1iw3fhmj4',
+    'ban_1me1onk3a11nw3kou14776fuyxtnmoatuqmpeioybffqx6okd53mo1iqmrym',
+    'ban_1mangozo4tnfq97hdtu8z9rdjsqnyo33i7o9aohoxsfbpx8kgwhrfu1rggj8',
 ];
 
 export const UPTIME_TRACKING_MIN_WEIGHT = 10_000;
