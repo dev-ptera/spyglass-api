1778731b07b85846e6f2692ad085b201e6c5fdf8
dev-ptera <dev.ptera@gmail.com>
2021-12-16 09:26:24 -0500

    Add comment

--- server/src/rpc/calls/accounts-pending.rpc.ts
+++ server/src/rpc/calls/accounts-pending.rpc.ts
@@ -1,6 +1,7 @@
 import { AccountsPendingResponse } from '@dev-ptera/nano-node-rpc';
 import { NANO_CLIENT } from '@app/config';
 
+/** Returns a list of receivable transactions for an account, sorted by balance descending. */
 export const accountsPendingRpc = async (addresses: string[]): Promise<AccountsPendingResponse> =>
     NANO_CLIENT.accounts_pending(addresses, -1, {
         source: false,
