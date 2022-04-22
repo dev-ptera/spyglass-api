f3d818a50311f4168dc0fe04811e21e9776f1797
dev-ptera <dev.ptera@gmail.com>
2022-04-22 12:32:43 -0400

    Add auth token for prussia

--- server/src/middleware/rateLimiter.ts
+++ server/src/middleware/rateLimiter.ts
@@ -1,10 +1,14 @@
+import { API_ACCESS_TOKENS } from './api-access-tokens';
 export const REQUESTS_PER_MINUTE = 50;
-// TODO: issue key and reduce to 10.
 
 const rateLimit = require('express-rate-limit');
 
 export const rateLimter = rateLimit({
     windowMs: 1 * 60 * 1000,
-    max: REQUESTS_PER_MINUTE,
+    max: async (request) => {
+        if (API_ACCESS_TOKENS.includes(request.get('Authorization'))) {
+            return 200;
+        } else return REQUESTS_PER_MINUTE;
+    },
     message: { error: 'Too many requests, please try again later.' },
 });
