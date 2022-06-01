51105361c04aaa5aca68c1f3264a2c8c267f2563
dev-ptera <dev.ptera@gmail.com>
2022-05-31 21:03:48 -0400

    Return empty arr by default

--- server/src/services/etc/file-reader.service.ts
+++ server/src/services/etc/file-reader.service.ts
@@ -1,11 +1,12 @@
 const fs = require('fs');
 
 /** Given a JSON file, reads file and returns parsed json object. */
-export const readFileContents = (file: string): any => {
+export const readFileContents = (file: string): any[] => {
     try {
         const data = fs.readFileSync(file, 'utf8');
         return JSON.parse(data);
     } catch (err) {
         console.error(err);
+        return [];
     }
 };
