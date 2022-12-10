275e9b51866c7cda9455fb3775b809455c9b763e
dev-ptera <dev.ptera@gmail.com>
2022-12-10 13:31:02 -0500

    Revert

--- server/src/app.ts
+++ server/src/app.ts
@@ -240,17 +240,17 @@ const server = http.createServer(app).listen(port, async () => {
     /* Updating the network metrics are now staggered so that during each reset interval, not all calls are fired at once.
      *  This will put a little less strain on the node running the API.  */
     void setRefreshIncrements([
-        //   onlineRepresentatives,
-        //  delegatorCount,
-        // priceData,
+        onlineRepresentatives,
+        delegatorCount,
+        priceData,
         monitoredRepresentatives,
         // This has to be called after the monitoredRepresentatives & onlineRepresentatives calls.
         // In V22, small reps are not online via rpc so use monitor software to mark as online.
-        // writeUptimePings,
-        //representativeScores,
-        //knownAccounts,
-        // socialMediaAccounts,
-        // accountsDistribution,
+        writeUptimePings,
+        representativeScores,
+        knownAccounts,
+        socialMediaAccounts,
+        accountsDistribution,
     ]);
 });
 expressWs(app, server);
